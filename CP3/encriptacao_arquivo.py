e7bb28dc471f1f16615807a125b787400eac89f7
Fabio <89427469+FabioMAmaral@users.noreply.github.com>
2024-05-10 19:53:38 -0300

    Update encriptacao_arquivo.py

--- CP3/encriptacao_arquivo.py
+++ CP3/encriptacao_arquivo.py
@@ -1,24 +1,50 @@
 from cryptography.fernet import Fernet
+import os
 
-# Gerar ou carregar a chave de criptografia
-# Neste exemplo, estamos gerando uma chave aleatória
-senha_encriptacao = Fernet.generate_key()
-fernet = Fernet(senha_encriptacao)
+def generate_key():
+    return Fernet.generate_key()
 
-def encriptar_arquivo(nome_arquivo, arquivo_encriptado):
-    with open(nome_arquivo, 'rb') as file:
-        arquivo = file.read()
-    arquivo_encriptado = fernet.encrypt(arquivo)
-    with open(arquivo_encriptado, 'wb') as encrypted_file:
-        encrypted_file.write(arquivo_encriptado)
+def save_key_to_file(key, filename='encryption_key.key'):
+    with open(filename, 'wb') as f:
+        f.write(key)
 
-def desencriptar_arquivo(arquivo_encriptado, arquivo_desencriptado):
-    with open(arquivo_encriptado, 'rb') as file:
-        arquivo_encriptado = file.read()
-    arquivo_desencriptado = fernet.decrypt(arquivo_encriptado)
-    with open(arquivo_desencriptado, 'wb') as decrypted_file:
-        decrypted_file.write(arquivo_desencriptado)
+def load_key_from_file(filename='encryption_key.key'):
+    with open(filename, 'rb') as f:
+        key = f.read()
+    return key
 
-# Exemplo de uso
-encriptar_arquivo('arquivo.txt', 'arquivo_encriptado.txt')
-desencriptar_arquivo('arquivo_encriptado.txt', 'arquivo_desencriptado.txt')
+def encrypt_file(filename, key):
+    with open(filename, 'rb') as f:
+        data = f.read()
+
+    fernet = Fernet(key)
+    encrypted_data = fernet.encrypt(data)
+
+    with open(filename + '.encrypted', 'wb') as f:
+        f.write(encrypted_data)
+
+def decrypt_file(encrypted_filename, key):
+    fernet = Fernet(key)
+
+    with open(encrypted_filename, 'rb') as f:
+        encrypted_data = f.read()
+
+    decrypted_data = fernet.decrypt(encrypted_data)
+
+    decrypted_filename = os.path.splitext(encrypted_filename)[0]
+    with open(decrypted_filename, 'wb') as f:
+        f.write(decrypted_data)
+
+encryption_key = generate_key()
+save_key_to_file(encryption_key)
+filename = 'arquivo.txt'
+
+encrypt_file(filename, encryption_key)
+print("Arquivo encriptado com sucesso!")
+
+encrypted_filename = filename + '.encrypted'
+password = input("Digite a senha para descriptografar o arquivo: ")
+
+password_encoded = password.encode()
+key = load_key_from_file()
+decrypt_file(encrypted_filename, key)
